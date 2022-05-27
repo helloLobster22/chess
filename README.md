2ab50ebce665d952409abd51a433f20d191b5e00
Natalia <nzemla10@gmail.com>
2022-05-27 17:32:57 +0200

    change for better

---
+++ README.md
@@ -0,0 +1,17 @@
+#  Chess AI
+Project realized as a part of a class of Data Analysis and Machine Learning.
+
+## Table of Contents
+
+-   General Info
+-   Technologies
+-   Setup
+
+## General Info
+Graphics: https://assetstore.unity.com/packages/3d/props/low-poly-chess-set-104629
+
+## Technologies
+-   Unity 2020.3.30f1
+-   C# 7.3
+
+## Setup
---
+++ chess/Assets/Scripts/ChessBoard.cs
@@ -0,0 +1,125 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChessBoard : MonoBehaviour
+{
+    [Header("Art")]
+    [SerializeField] private Material tileMaterial;
+    [SerializeField] private float tileSize = 1.0f;
+    [SerializeField] private float yOffset = 0.2f;
+    [SerializeField] private Vector3 boardCenter = Vector3.zero;
+
+    [Header("Prefabs & Materials")]
+    [SerializeField] private GameObject[] prefabs;
+    [SerializeField] private Material[] teamMaterials;
+ 
+    private const int TILE_X = 8;
+    private const int TILE_Y = 8;
+    private GameObject[,] tiles;
+    private Camera currentCamera;
+    private Vector2Int currentHover;
+    private Vector3 bounds;
+
+
+    private void Awake()
+    {
+        GenerateAllTiles(tileSize, TILE_X, TILE_Y);
+    }
+
+    private void Update()
+    {
+        if (!currentCamera)
+        {
+            currentCamera = Camera.main;
+            return;
+        }
+
+        RaycastHit info;
+        Ray ray = currentCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out info, 100, LayerMask.GetMask("Tile"))) 
+        {
+            // Get the indexes of the tile hitted
+            Vector2Int hitPosition = LookupTileIndex(info.transform.gameObject);
+
+            // If we're hovering a tile after not hovering any tiles
+            if(currentHover == -Vector2Int.one)
+            {
+                currentHover = hitPosition;
+                tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+            }
+
+            // If we were already hovering a tile, change the previous one
+            if (currentHover != hitPosition)
+            {
+                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                currentHover = hitPosition;
+                tiles[hitPosition.x, hitPosition.y].layer = LayerMask.NameToLayer("Hover");
+            }
+        }
+        else
+        {
+            if (currentHover != -Vector2Int.one)
+            {
+                tiles[currentHover.x, currentHover.y].layer = LayerMask.NameToLayer("Tile");
+                currentHover = -Vector2Int.one;
+            }
+        }
+     }
+
+
+    // Generate the board
+    private void GenerateAllTiles(float tileSize, int tileX, int tileY)
+    {
+        yOffset += transform.position.y;
+        bounds = new Vector3((tileX / 2) * tileSize, 0, (tileX / 2) * tileSize) + boardCenter;
+
+        tiles = new GameObject[tileX, tileY];
+
+        for (int x = 0; x < tileX; x++)
+            for (int y = 0; y < tileY; y++)
+                tiles[x, y] = GenerateSingleTiles(tileSize, x, y);
+    }
+
+    private GameObject GenerateSingleTiles(float tileSize, int x, int y)
+    {
+        GameObject tileObject = new GameObject(string.Format("X:{0}. Y:{1}", x, y));
+        tileObject.transform.parent = transform;
+
+        Mesh mesh = new Mesh();
+        tileObject.AddComponent<MeshFilter>().mesh = mesh;
+        tileObject.AddComponent<MeshRenderer>().material = tileMaterial;
+
+        Vector3[] vertices = new Vector3[4];
+        vertices[0] = new Vector3(x * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[1] = new Vector3(x * tileSize, yOffset, (y + 1) * tileSize) - bounds;
+        vertices[2] = new Vector3((x + 1) * tileSize, yOffset, y * tileSize) - bounds;
+        vertices[3] = new Vector3((x + 1) * tileSize, yOffset, (y + 1) * tileSize) - bounds;
+
+        int[] tris = new int[] { 0, 1, 2, 1, 3, 2 };
+
+        mesh.vertices = vertices;
+        mesh.triangles = tris;
+        mesh.RecalculateNormals();
+
+        tileObject.layer = LayerMask.NameToLayer("Tile");
+        tileObject.AddComponent<BoxCollider>();
+
+        tileObject.AddComponent<BoxCollider>();
+
+        return tileObject;
+    }
+
+    // Operations
+    private Vector2Int LookupTileIndex(GameObject hitInfo)
+    {
+        for (int x = 0; x < TILE_X; x++)
+            for (int y = 0; y < TILE_Y; y++)
+                if (tiles[x, y] == hitInfo)
+                    return new Vector2Int(x, y);
+
+        return -Vector2Int.one; // -1, -1
+    }
+}
+
